ebca2c33f7fb9f10d04a311cfe3d8e43dd4a0a70
Karan Gupta <karan@Karans-MacBook-Pro.local>
2023-12-09 22:07:03 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./dist/*.html"],
   theme: {
     extend: {},
+
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1480px',
+      },
   },
   plugins: [],
 }
-
